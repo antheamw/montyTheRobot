7952ec8d9246ab0cfe3d159f55f96d750c996b20
antheamw <anthea.m.wong@hotmail.com>
2016-04-04 16:01:39 -0700

    Create proximityv1.c

---
+++ proximityv1.c
@@ -0,0 +1,157 @@
+// adcMultiInputandPeriodv4.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <c8051f38x.h>
+#include <math.h>
+
+// ANSI colors
+#define	COLOR_BLACK		0
+#define	COLOR_RED		1
+#define	COLOR_GREEN		2
+#define	COLOR_YELLOW	3
+#define	COLOR_BLUE		4
+#define	COLOR_MAGENTA	5
+#define	COLOR_CYAN		6
+#define	COLOR_WHITE		7
+
+// Some ANSI escape sequences
+#define CURSOR_ON "\x1b[?25h"
+#define CURSOR_OFF "\x1b[?25l"
+#define CLEAR_SCREEN "\x1b[2J"
+#define GOTO_YX "\x1B[%d;%dH"
+#define CLR_TO_END_LINE "\x1B[K"
+
+#define MHZ 1000000L
+#define SYSCLK (24*MHZ)
+#define BAUDRATE 115200L
+#define TRIG P2_6
+#define ECHO P2_5
+unsigned char overflow_count;
+
+char _c51_external_startup (void)
+{
+	PCA0MD&=(~0x40) ;    // DISABLE WDT: clear Watchdog Enable bit
+	// CLKSEL&=0b_1111_1000; // Not needed because CLKSEL==0 after reset
+	#if (SYSCLK == (12*MHZ))
+		//CLKSEL|=0b_0000_0000;  // SYSCLK derived from the Internal High-Frequency Oscillator / 4 
+	#elif (SYSCLK == (24*MHZ))
+		CLKSEL|=0b_0000_0010; // SYSCLK derived from the Internal High-Frequency Oscillator / 2.
+	#elif (SYSCLK == (48*MHZ))
+		CLKSEL|=0b_0000_0011; // SYSCLK derived from the Internal High-Frequency Oscillator / 1.
+	#else
+		#error SYSCLK must be either 12MHZ, 24MHZ, or 48MHZ
+	#endif
+	OSCICN |= 0x03; // Configure internal oscillator for its maximum frequency
+	
+	P2MDIN = 0b_0000_0000;
+	P2MDOUT=0b_0100_0000;     // Enable Uart TX as push-pull output
+	XBR0=0x01;         // Enable UART on P0.4(TX) and P0.5(RX)
+	XBR1=0x40; // Enable crossbar.  Enable T0 input.
+	XBR2=0b_0000_0000;
+		SCON0 = 0x10;
+	#if (SYSCLK/BAUDRATE/2L/256L < 1)
+		TH1 = 0x10000-((SYSCLK/BAUDRATE)/2L);
+		CKCON &= ~0x0B;                  // T1M = 1; SCA1:0 = xx
+		CKCON |=  0x08;
+	#elif (SYSCLK/BAUDRATE/2L/256L < 4)
+		TH1 = 0x10000-(SYSCLK/BAUDRATE/2L/4L);
+		CKCON &= ~0x0B; // T1M = 0; SCA1:0 = 01                  
+		CKCON |=  0x01;
+	#elif (SYSCLK/BAUDRATE/2L/256L < 12)
+		TH1 = 0x10000-(SYSCLK/BAUDRATE/2L/12L);
+		CKCON &= ~0x0B; // T1M = 0; SCA1:0 = 00
+	#else
+		TH1 = 0x10000-(SYSCLK/BAUDRATE/2/48);
+		CKCON &= ~0x0B; // T1M = 0; SCA1:0 = 10
+		CKCON |=  0x02;
+	#endif
+	
+	TL1 = TH1;     // Init timer 1
+	TMOD &= 0x0f;  // TMOD: timer 1 in 8-bit autoreload
+	TMOD |= 0x20;                       
+	TR1 = 1;       // Start timer1
+	SCON = 0x52;
+	
+	return 0;
+}
+void TIMER0_Init(void)
+{
+	TMOD&=0b_1111_0000; // Set the bits of Timer/Counter 0 to zero
+	TMOD|=0b_0000_0001; // Timer/Counter 0 used as a 16-bit timer
+	TR0=0; // Stop Timer/Counter 0
+}
+
+// Uses Timer3 to delay <us> micro-seconds. 
+void Timer3us(unsigned char us)
+{
+	unsigned char i;               // usec counter
+	
+	// The input for Timer 3 is selected as SYSCLK by setting T3ML (bit 6) of CKCON:
+	CKCON|=0b_0100_0000;
+	
+	TMR3RL = (-(SYSCLK)/1000000L); // Set Timer3 to overflow in 1us.
+	TMR3 = TMR3RL;                 // Initialize Timer3 for first overflow
+	
+	TMR3CN = 0x04;                 // Sart Timer3 and clear overflow flag
+	for (i = 0; i < us; i++)       // Count <us> overflows
+	{
+		while (!(TMR3CN & 0x80));  // Wait for overflow
+		TMR3CN &= ~(0x80);         // Clear overflow indicator
+	}
+	TMR3CN = 0 ;                   // Stop Timer3 and clear overflow flag
+}
+
+void waitms (unsigned int ms)
+{
+	unsigned int j;
+	unsigned char k;
+	for(j=0; j<ms; j++)
+		for (k=0; k<4; k++) Timer3us(250);
+}
+
+#define VDD      3.378 // The measured value of VDD in volts
+#define NUM_INS  2
+
+void main (void)
+{
+	float duration = 0;
+	float distance = 0;
+	int i = 0;
+	P2_5 = 1;
+	
+	PCA0MD &= ~0x40; // WDTE = 0 (clear watchdog timer enable)
+	_c51_external_startup();
+	TIMER0_Init();
+	
+	while(1){
+		TL0=0; 
+		TH0=0;
+		TF0=0;
+		overflow_count=0;
+		P2_6 = 0;
+		Timer3us(2);
+		P2_6 = 1;
+		Timer3us(10);
+//		printf("%d", i);
+		P2_6 = 0;
+		while(P2_5 == 0){
+			i++;
+		}
+		TR0=1; // Start timing
+		while (P2_5 == 1){
+			if(TF0==1) // Did the 16-bit timer overflow?
+				{
+					TF0=0;
+					overflow_count++;
+				}
+		}
+		TR0 = 0;
+		duration=(overflow_count*65536.0+TH0*256.0+TL0)*(12.0/SYSCLK);
+		distance = (duration / 2)/29;
+		printf("%.9fs %.9f cm\n", duration, distance);
+
+		waitms(50);
+	}
+	 
+}
